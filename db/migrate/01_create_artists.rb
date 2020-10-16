0c3d7d461481486806bc91e221a1d1c438494ca0
wlytle <wlytle89@gmail.com>
2020-10-15 20:24:22 -0700

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,16 @@
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+
+  def up
+  end
+
+  def down
+  end
+end
